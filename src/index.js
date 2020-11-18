c50ff9a483be940871c691d38e96ccaed4254080
nathanpriyasadie <nathanpriyasadie@gmail.com>
2020-11-18 20:49:52 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './styles/styles.css'
+import './styles/styles.scss'
+
+const App = () => {
+    return <h1>Hello From Jakarta</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
